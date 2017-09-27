ced85ee17c72699e16fc72f9935b46b5d26acb85
yaoyongchao <yaoyongchao3g@163.com>
2017-09-27 17:29:12 +0800

    Initial commit

---
+++ App.js
@@ -0,0 +1,26 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View
+} from 'react-native';
+
+export default class App extends Component {
+    render() {
+        return (
+            <View>
+                <Text>
+                    Welcome to React Native!
+                </Text>
+
+            </View>
+        );
+    }
+}
